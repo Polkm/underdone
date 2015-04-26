25740536c1a5a848248ec8fcc2ecbfecabd3c31f
Matthew Nichols <mnichols@umass.edu>
2015-04-26 04:28:45 -0400

    Taken Credit

--- gamemode/core/clientfiles/menutabs/helpmenu/help.lua
+++ gamemode/core/clientfiles/menutabs/helpmenu/help.lua
@@ -12,13 +12,13 @@ end
 
 function PANEL:CreateHelp()
   tblFAQ[1] = {Text = "What is this?", Color = clrWhite}
-  tblFAQ[2] = {Text = "This is a gamemode called underdone, It's an RPG by the Shell Shocked Gaming and Tower Yard Entitertainment Team.", Color = clrDrakGray}
+  tblFAQ[2] = {Text = "This is a gamemode called underdone, It's an RPG by @Polkm1.", Color = clrDrakGray}
   tblFAQ[3] = {Text = "What do I do?", Color = clrWhite}
   tblFAQ[4] = {Text = "You can do many things in underdone, such as doing quests, killing npcs, and collecting rare items. Play it as you would an normal RPG.", Color = clrDrakGray}
   tblFAQ[5] = {Text = "How do I get money?", Color = clrWhite}
   tblFAQ[7] = {Text = "Unlike some other gamemodes you might be famillar with underdone does not jsut give you money, You are expected to earn it through quests, selling items, and picking up droped items.", Color = clrDrakGray}
   tblFAQ[8] = {Text = "How do I open the Inventory?", Color = clrWhite}
-  tblFAQ[9] = {Text = "To open your main menu containign the inventory and skills and players tabs press and hold the Q button, as you would in sandbox", Color = clrDrakGray}
+  tblFAQ[9] = {Text = "To open your main menu containing the inventory and skills and players tabs press and hold the Q button, as you would in sandbox.", Color = clrDrakGray}
   tblFAQ[10] = {Text = "How do I use my Skill Points?", Color = clrWhite}
   tblFAQ[11] = {Text = "If you open your main mennu (see above) you will see a skills tab, goto that panel and you can see the full sellection of skills available. If you can not get a skill it will apear grayed out. To spend a Skill Point double click the icon of the skill you woudl like to get.", Color = clrDrakGray}
 end
@@ -30,6 +30,3 @@ function PANEL:LoadHelp()
   end
 end
 vgui.Register("helptab", PANEL, "Panel")
-
-
-
